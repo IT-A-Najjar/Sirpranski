8e7b4d0e89212761bf9688896f2686affe653298
Abdullah Najjar <eng.abdullah.najjar.2001@gmail.com>
2022-09-17 10:48:21 +0300

    ok

---
+++ sirpranski/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace sirpranski
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.canves = new System.Windows.Forms.PictureBox();
+            this.tb_scale = new System.Windows.Forms.TrackBar();
+            this.tb_poly = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.canves)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_scale)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_poly)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 457);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "sirpranski triangle",
+            "sirpranski polygon"});
+            this.comboBox1.Location = new System.Drawing.Point(561, 467);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(209, 27);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(254, 467);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "level";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(338, 457);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 37);
+            this.textBox1.TabIndex = 4;
+            // 
+            // canves
+            // 
+            this.canves.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.canves.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canves.Location = new System.Drawing.Point(12, 12);
+            this.canves.Name = "canves";
+            this.canves.Size = new System.Drawing.Size(564, 439);
+            this.canves.TabIndex = 6;
+            this.canves.TabStop = false;
+            // 
+            // tb_scale
+            // 
+            this.tb_scale.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_scale.Location = new System.Drawing.Point(701, 51);
+            this.tb_scale.Maximum = 100;
+            this.tb_scale.Minimum = 1;
+            this.tb_scale.Name = "tb_scale";
+            this.tb_scale.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.tb_scale.Size = new System.Drawing.Size(69, 400);
+            this.tb_scale.TabIndex = 7;
+            this.tb_scale.Value = 1;
+            // 
+            // tb_poly
+            // 
+            this.tb_poly.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_poly.Location = new System.Drawing.Point(603, 51);
+            this.tb_poly.Maximum = 15;
+            this.tb_poly.Minimum = 3;
+            this.tb_poly.Name = "tb_poly";
+            this.tb_poly.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.tb_poly.Size = new System.Drawing.Size(69, 400);
+            this.tb_poly.TabIndex = 8;
+            this.tb_poly.Value = 3;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(599, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 19);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "polygon";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(697, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 19);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "scaling";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 506);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_poly);
+            this.Controls.Add(this.tb_scale);
+            this.Controls.Add(this.canves);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "sirpranski";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.canves)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_scale)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_poly)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox canves;
+        private System.Windows.Forms.TrackBar tb_scale;
+        private System.Windows.Forms.TrackBar tb_poly;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ sirpranski/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sirpranski
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void draw_triangle(Graphics graphic,PointF top,PointF left,PointF right)
+        {
+            Pen pen = new Pen(Brushes.Black);
+            pen.Width = 1;
+
+            graphic.DrawPolygon(pen, new PointF[] { top, left, right });
+        }
+
+        void call_s_triangel(Graphics graphic,PointF top,PointF left,PointF right,int level)
+        {
+            if (level == 0)
+            {
+                draw_triangle(graphic, top, left, right);
+            }
+            else
+            {
+                PointF mid_left = new PointF((top.X + left.X) / 2, (top.Y + left.Y) / 2);
+                PointF mid_right = new PointF((top.X + right.X) / 2, (top.Y + right.Y) / 2);
+                PointF mid_bottom = new PointF((right.X + left.X) / 2, (right.Y + left.Y) / 2);
+
+                call_s_triangel(graphic, top, mid_left, mid_right, level - 1);
+                call_s_triangel(graphic, mid_left, left, mid_bottom, level - 1);
+                call_s_triangel(graphic, mid_right, mid_bottom, right, level - 1);
+            }
+        }
+        PointF[] find_pentagon_points_general(PointF center,float radius,int number)
+        {
+            PointF[] points = new PointF[number];
+            double current_angel = -Math.PI / 2;
+            double move = (2 * Math.PI) / number;
+
+            for(int i = 0; i < number; i++)
+            {
+                points[i] = new PointF(center.X + (float)(radius * Math.Cos(current_angel)), center.Y + (float)(radius * Math.Sin(current_angel)));
+                current_angel += move;
+            }
+            return points;
+        }
+
+        void call_s_polygon(Graphics graphic, PointF center, float radius, int level)
+        {
+            PointF[] points;
+            float scale = tb_scale.Value / 200.0f;
+            int polygons = tb_poly.Value;
+
+            if (level == 0)
+            {
+                points = find_pentagon_points_general(center, radius, polygons);
+                graphic.FillPolygon(Brushes.Green, points);
+            }
+            else
+            {
+                float subtract_radius = radius - (radius * scale);
+                points = find_pentagon_points_general(center, subtract_radius, polygons);
+                for (int i = 0; i < polygons; i++)
+                {
+                    call_s_polygon(graphic, points[i], (radius * scale), level - 1);
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Bitmap bitmap = new Bitmap(canves.ClientSize.Width, canves.ClientSize.Height);
+            canves.Image = bitmap;
+
+            using (Graphics graphic = Graphics.FromImage(bitmap))
+            {
+                graphic.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                graphic.Clear(canves.BackColor);
+                int level = Convert.ToInt32(textBox1.Text);
+
+                if(comboBox1.Text== "sirpranski triangle")
+                {
+                    PointF top = new PointF((canves.Width / 2f), 15);
+                    PointF left = new PointF(15, canves.Height - 15);
+                    PointF right = new PointF(canves.Width - 15, canves.Height - 15);
+                     
+                    call_s_triangel(graphic, top, left, right, level);
+                }
+                else if(comboBox1.Text== "sirpranski polygon")
+                {
+                    PointF center = new PointF(canves.ClientSize.Width / 2, canves.ClientSize.Height / 2);
+
+                    float radius = Math.Min(center.X, center.Y) - 20;
+                    call_s_polygon(graphic, center, radius, level);
+                }
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tb_poly.Enabled = false;
+            tb_scale.Enabled = false;
+            if(comboBox1.Text== "sirpranski polygon")
+            {
+                tb_poly.Enabled = true;
+                tb_scale.Enabled = true;
+            }
+        }
+    }
+}
